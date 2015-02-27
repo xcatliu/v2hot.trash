ce6276dd90642cc35abd19707511ba3450e5013f
xcatliu <xcatliu@gmail.com>
2015-02-27 21:38:32 +0800

    init react-native

---
+++ index.ios.js
@@ -0,0 +1,64 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var v2hot = React.createClass({
+  componentDidMount: function() {
+    fetch('https://www.v2ex.com/api/topics/hot.json')
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log(responseData);
+      })
+      .done();
+  },
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Hello World!
+        </Text>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+Control+Z for dev menu
+        </Text>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('v2hot', () => v2hot);
