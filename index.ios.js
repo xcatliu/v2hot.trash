085ea15a866d00b54019b48a2cbeb9a634f5aa6c
xcatliu <xcatliu@gmail.com>
2015-03-09 23:26:01 +0800

    navigationIOS

--- index.ios.js
+++ index.ios.js
@@ -1,71 +1,214 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
 var React = require('react-native');
+var ViewExample = require('./ViewExample');
+var createExamplePage = require('./createExamplePage');
 var {
-  AppRegistry,
+  PixelRatio,
+  ScrollView,
   StyleSheet,
   Text,
+  TouchableHighlight,
   View,
-  ListView,
-  Navigator
 } = React;
 
-var v2hot = React.createClass({
-  getInitialState: function() {
-    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    return {
-      dataSource: ds.cloneWithRows(['row 1', 'row 2', 'row 3', 'row 2', 'row 2', 'row 2']),
-    };
-  },
-  componentDidMount: function() {
-    fetch('https://www.v2ex.com/api/topics/hot.json')
-      .then((response) => response.json())
-      .then((responseData) => {
-        this.setState({
-          topics: responseData
-        });
-      })
-      .done();
-  },
+var EmptyPage = React.createClass({
+
   render: function() {
     return (
-      <Navigator
-        initialRoute={{name: 'My First Scene', index: 0}}
-        renderScene={(route, navigator) =>
-          <ListView
-            style={styles.listView}
-            dataSource={this.state.dataSource}
-            renderRow={(rowData) => <Text>Hello World</Text>}
-          />
-        }
-      />
+      <View style={styles.emptyPage}>
+        <Text style={styles.emptyPageText}>
+          {this.props.text}
+        </Text>
+      </View>
     );
-  }
+  },
+
 });
-var styles = StyleSheet.create({
-  listView: {
-    backgroundColor: '#FFF'
+
+var NavigatorIOSExample = React.createClass({
+
+  statics: {
+    title: '<NavigatorIOS>',
+    description: 'iOS navigation capabilities',
   },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+
+  render: function() {
+    var recurseTitle = 'Recurse Navigation';
+    if (!this.props.topExampleRoute) {
+      recurseTitle += ' - more examples here';
+    }
+    return (
+      <ScrollView style={styles.list}>
+        <View style={styles.line}/>
+        <View style={styles.group}>
+          <View style={styles.row}>
+            <Text style={styles.rowNote}>
+              See &lt;UIExplorerApp&gt; for top-level usage.
+            </Text>
+          </View>
+        </View>
+        <View style={styles.line}/>
+        <View style={styles.groupSpace}/>
+        <View style={styles.line}/>
+        <View style={styles.group}>
+          {this._renderRow(recurseTitle, () => {
+            this.props.navigator.push({
+              title: NavigatorIOSExample.title,
+              component: NavigatorIOSExample,
+              backButtonTitle: 'Custom Back',
+              passProps: {topExampleRoute: this.props.topExampleRoute || this.props.route},
+            });
+          })}
+          {this._renderRow('Push View Example', () => {
+            this.props.navigator.push({
+              title: 'Very Long Custom View Example Title',
+              component: createExamplePage(null, ViewExample),
+            });
+          })}
+          {this._renderRow('Custom Right Button', () => {
+            this.props.navigator.push({
+              title: NavigatorIOSExample.title,
+              component: EmptyPage,
+              rightButtonTitle: 'Cancel',
+              onRightButtonPress: () => this.props.navigator.pop(),
+              passProps: {
+                text: 'This page has a right button in the nav bar',
+              }
+            });
+          })}
+          {this._renderRow('Pop', () => {
+            this.props.navigator.pop();
+          })}
+          {this._renderRow('Pop to top', () => {
+            this.props.navigator.popToTop();
+          })}
+          {this._renderRow('Replace here', () => {
+            var prevRoute = this.props.route;
+            this.props.navigator.replace({
+              title: 'New Navigation',
+              component: EmptyPage,
+              rightButtonTitle: 'Undo',
+              onRightButtonPress: () => this.props.navigator.replace(prevRoute),
+              passProps: {
+                text: 'The component is replaced, but there is currently no ' +
+                  'way to change the right button or title of the current route',
+              }
+            });
+          })}
+          {this._renderReplacePrevious()}
+          {this._renderReplacePreviousAndPop()}
+          {this._renderPopToTopNavExample()}
+        </View>
+        <View style={styles.line}/>
+      </ScrollView>
+    );
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+
+  _renderPopToTopNavExample: function() {
+    if (!this.props.topExampleRoute) {
+      return null;
+    }
+    return this._renderRow('Pop to top NavigatorIOSExample', () => {
+      this.props.navigator.popToRoute(this.props.topExampleRoute);
+    });
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+
+  _renderReplacePrevious: function() {
+    if (!this.props.topExampleRoute) {
+      // this is to avoid replacing the UIExplorerList at the top of the stack
+      return null;
+    }
+    return this._renderRow('Replace previous', () => {
+      this.props.navigator.replacePrevious({
+        title: 'Replaced',
+        component: EmptyPage,
+        passProps: {
+          text: 'This is a replaced "previous" page',
+        },
+        wrapperStyle: styles.customWrapperStyle,
+      });
+    });
+  },
+
+  _renderReplacePreviousAndPop: function() {
+    if (!this.props.topExampleRoute) {
+      // this is to avoid replacing the UIExplorerList at the top of the stack
+      return null;
+    }
+    return this._renderRow('Replace previous and pop', () => {
+      this.props.navigator.replacePreviousAndPop({
+        title: 'Replaced and Popped',
+        component: EmptyPage,
+        passProps: {
+          text: 'This is a replaced "previous" page',
+        },
+        wrapperStyle: styles.customWrapperStyle,
+      });
+    });
+  },
+
+  _renderRow: function(title: string, onPress: Function) {
+    return (
+      <View>
+        <TouchableHighlight onPress={onPress}>
+          <View style={styles.row}>
+            <Text style={styles.rowText}>
+              {title}
+            </Text>
+          </View>
+        </TouchableHighlight>
+        <View style={styles.separator} />
+      </View>
+    );
   },
 });
 
-AppRegistry.registerComponent('v2hot', () => v2hot);
+var styles = StyleSheet.create({
+  customWrapperStyle: {
+    backgroundColor: '#bbdddd',
+  },
+  emptyPage: {
+    flex: 1,
+    paddingTop: 64,
+  },
+  emptyPageText: {
+    margin: 10,
+  },
+  list: {
+    backgroundColor: '#eeeeee',
+    marginTop: 10,
+  },
+  group: {
+    backgroundColor: 'white',
+  },
+  groupSpace: {
+    height: 15,
+  },
+  line: {
+    backgroundColor: '#bbbbbb',
+    height: 1 / PixelRatio.get(),
+  },
+  row: {
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+  },
+  separator: {
+    height: 1 / PixelRatio.get(),
+    backgroundColor: '#bbbbbb',
+    marginLeft: 15,
+  },
+  rowNote: {
+    fontSize: 17,
+  },
+  rowText: {
+    fontSize: 17,
+    fontWeight: '500',
+  },
+});
+
+AppRegistry.registerComponent('v2hot', () => NavigatorIOSExample);
+
+module.exports = NavigatorIOSExample;
