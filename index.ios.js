db9d324f67af853f828d722a0900de4005d8049e
xcatliu <xcatliu@gmail.com>
2015-03-24 22:57:42 +0800

    update url

--- index.ios.js
+++ index.ios.js
@@ -10,7 +10,7 @@ var React = require('react-native');
  * For quota reasons we replaced the Rotten Tomatoes' API with a sample data of
  * their very own API that lives in React Native's Github repo.
  */
-var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+var REQUEST_URL = 'https://www.v2ex.com/api/topics/hot.json';
 
 var MOCKED_MOVIES_DATA = [
   {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
