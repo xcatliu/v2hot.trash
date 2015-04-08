1f446d80142da444cefff1be98e4c7cab54a37e1
xcatliu <xcatliu@gmail.com>
2015-04-08 22:34:32 +0800

    update Topics

--- Topics.js
+++ Topics.js
@@ -2,7 +2,27 @@
 
 var React = require('react-native');
 
+var REQUEST_URL = 'https://www.v2ex.com/api/topics/hot.json';
+
+var {
+  Image,
+  ListView,
+  StyleSheet,
+  Text,
+  View,
+  NavigatorIOS,
+  TouchableHighlight
+} = React;
+
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  },
   render: function() {
     return (
       <NavigatorIOS
